7f63a79e23903af8937888289c81edf444cfe259
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-12-13 22:46:54 +0100

    feat: add mock product data for development

---
+++ frontend/src/products.ts
@@ -0,0 +1,29 @@
+const products = [
+	{
+		_id: '1',
+		name: 'iPhone 16 Pro Max',
+		image: '/images/phone.webp',
+		description: `The iPhone 16 Pro Max features the powerful A18 Pro chip, delivering top-tier performance for gaming and computational tasks, along with enhanced battery efficiency. Its camera system includes a 48MP Ultra Wide lens for detailed macro and landscape shots, advanced photographic styles, and 4K 120fps video recording for exceptional visuals and creative flexibilit`,
+		brand: 'Apple',
+		category: 'Electronics',
+		price: 4000.0,
+		countInStock: 7,
+		rating: 4.5,
+		numReviews: 8,
+	},
+	{
+		_id: '2',
+		name: 'MacBook Pro Max',
+		image: '/images/laptop.webp',
+		description:
+			'The latest MacBook Pro Max is powered by the M3 Max chip, offering groundbreaking speed and energy efficiency with up to 128GB unified memory for demanding workflows. Its Liquid Retina XDR display supports high dynamic range and ProMotion for stunning visuals, while the long battery life ensures all-day productivity',
+		brand: 'Apple',
+		category: 'Electronics',
+		price: 6000.0,
+		countInStock: 5,
+		rating: 4.8,
+		numReviews: 5,
+	},
+];
+
+export default products;
