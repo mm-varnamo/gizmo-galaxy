b6bd5f9e3e27d62f275a4d8a3b13fcdce013f6b1
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-12-21 18:58:13 +0100

    feat: add database seeding (import and destroy) functionality

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,10 +19,10 @@ app.get('/api/products', (req, res) => {
 	res.json(products);
 });
 
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((product) => product._id === req.params.id);
-	res.json(product);
-});
+// app.get('/api/products/:id', (req, res) => {
+// 	const product = products.find((product) => product._id === req.params.id);
+// 	res.json(product);
+// });
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`);
