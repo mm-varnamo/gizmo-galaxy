11cdaa939a7bbd60dc197e4ce4255768854a88a4
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-01-01 20:07:28 +0100

    feat: display total product count next to cart icon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { Cart } from '../models/cart';
 
 const Header = () => {
+	const { cartItems } = useSelector((state: RootState) => state.cart);
+	console.log(cartItems);
+
 	return (
 		<header>
 			<nav>
@@ -11,7 +18,16 @@ const Header = () => {
 						</Link>
 					</div>
 					<ul>
-						<li></li>
+						<li>
+							<Link to='/cart'>
+								<FaShoppingCart /> Cart
+								{cartItems.length > 0 &&
+									cartItems.reduce(
+										(acc: number, curr: Cart) => acc + curr.qty,
+										0
+									)}
+							</Link>
+						</li>
 						<li></li>
 						<li></li>
 					</ul>
