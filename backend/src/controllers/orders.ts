8ad25f3a2fb7827c9ce13df34cf773a35ae884c9
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-02-17 12:31:26 +0100

    feat: add order controllers functionality for creating and fetching the users orders

--- backend/src/controllers/orders.ts
+++ backend/src/controllers/orders.ts
@@ -5,7 +5,38 @@ import Order from '../models/orderModel';
 
 export const addOrderItems: RequestHandler = async (req, res, next) => {
 	try {
-		res.send('add order items');
+		const {
+			orderItems,
+			shippingAddress,
+			paymentMethod,
+			itemsPrice,
+			taxPrice,
+			shippingPrice,
+			totalPrice,
+		} = req.body;
+
+		if (orderItems && orderItems.length === 0) {
+			throw createHttpError(400, 'No order items');
+		} else {
+			const order = new Order({
+				orderItems: orderItems.map((item) => ({
+					...item,
+					product: item._id,
+					_id: undefined,
+				})),
+				user: req.user?.id,
+				shippingAddress,
+				paymentMethod,
+				itemsPrice,
+				taxPrice,
+				shippingPrice,
+				totalPrice,
+			});
+
+			const createdOrder = await order.save();
+
+			res.status(201).json(createdOrder);
+		}
 	} catch (error) {
 		next(error);
 	}
@@ -13,7 +44,8 @@ export const addOrderItems: RequestHandler = async (req, res, next) => {
 
 export const getMyOrders: RequestHandler = async (req, res, next) => {
 	try {
-		res.send('get my orders');
+		const orders = await Order.find({ user: req.user?.id });
+		res.status(200).json(orders);
 	} catch (error) {
 		next(error);
 	}
@@ -21,7 +53,16 @@ export const getMyOrders: RequestHandler = async (req, res, next) => {
 
 export const getOrderById: RequestHandler = async (req, res, next) => {
 	try {
-		res.send('get order by id');
+		const order = await Order.findById(req.params.id).populate(
+			'user',
+			'name email'
+		);
+
+		if (order) {
+			res.status(200).json(order);
+		} else {
+			throw createHttpError(404, 'Order not found');
+		}
 	} catch (error) {
 		next(error);
 	}
