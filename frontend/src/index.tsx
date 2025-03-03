d12993977fcc998e3ab30458f832ab59b38dbc65
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-03 23:49:38 +0100

    feat: add pagination routes for ProductListPage and HomePage

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -34,6 +34,7 @@ const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
 			<Route index={true} path='/' element={<HomePage />} />
+			<Route path='/page/:pageNumber' element={<HomePage />} />
 			<Route path='/product/:id' element={<ProductPage />} />
 			<Route path='/cart' element={<CartPage />} />
 			<Route path='/login' element={<LoginPage />} />
@@ -50,6 +51,10 @@ const router = createBrowserRouter(
 			<Route path='' element={<AdminRoute />}>
 				<Route path='/admin/orderlist' element={<OrderListPage />} />
 				<Route path='/admin/productlist' element={<ProductListPage />} />
+				<Route
+					path='/admin/productlist/:pageNumber'
+					element={<ProductListPage />}
+				/>
 				<Route path='/admin/product/:id/edit' element={<ProductEditPage />} />
 				<Route path='/admin/userlist' element={<UserListPage />} />
 				<Route path='/admin/user/:id/edit' element={<UserEditPage />} />
