faf0474b38954284125cac80e2c7a5d40ba7f53f
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-05 19:19:42 +0100

    feat: add dynamic pagination based on the presence of a search keyword in PaginationLinks component

--- frontend/src/components/PaginationLinks.tsx
+++ frontend/src/components/PaginationLinks.tsx
@@ -4,19 +4,27 @@ interface PaginationLinksProps {
 	pages: number;
 	activePage: number;
 	isAdmin?: boolean;
+	keyword?: string;
 }
 
 const PaginationLinks = ({
 	pages,
 	activePage,
 	isAdmin = false,
+	keyword = '',
 }: PaginationLinksProps) => {
 	return pages > 1 ? (
 		<div>
 			{Array.from({ length: pages }, (_, page) => (
 				<NavLink
 					key={page}
-					to={!isAdmin ? `/page/${page + 1}` : `/admin/productlist/${page + 1}`}
+					to={
+						!isAdmin
+							? keyword
+								? `/search/${keyword}/page/${page + 1}`
+								: `/page/${page + 1}`
+							: `/admin/productlist/${page + 1}`
+					}
 					className={
 						Number(activePage) - 1 === Number(page)
 							? 'active-link'
